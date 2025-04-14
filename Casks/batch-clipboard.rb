58f1b92c3d7e9f813641c5f01642daebe01c3914
Pierre Houston <jpmhouston+github@gmail.com>
2025-04-13 23:12:05 -0700

    Fixed test-bot warnings for batchclipboard cask

--- Casks/batch-clipboard.rb
+++ Casks/batch-clipboard.rb
@@ -1,19 +1,24 @@
 cask "batch-clipboard" do
   version "1.0.1b10"
   sha256 "0f348c96aa4452fa4d41491b875164fd6122672b6651e84b8df862a17131957e"
+
   url "https://github.com/jpmhouston/Cleepp/releases/download/v#{version}/Batch.Clipboard.#{version}.zip", verified: "github.com/jpmhouston/Cleepp/releases/download/"
   name "Batch Clipboard"
   desc "Menubar app adding easy clipboard queue feature"
   homepage "https://bananameter.lol/batchclipboard"
+
   livecheck do
     url "https://raw.githubusercontent.com/jpmhouston/Cleepp/forkmain/appcast.xml"
     strategy :sparkle, &:short_versio
   end
+
   depends_on macos: ">= :big_sur"
+
   app "Batch Clipboard.app"
+
   zap trash: [
-    "~/Library/Preferences/lol.bananameter.batchclip.plist",
+    "~/Library/Caches/lol.bananameter.batchclip",
     "~/Library/Containers/lol.bananameter.batchclip",
-    "~/Library/Caches/lol.bananameter.batchclip"    
+    "~/Library/Preferences/lol.bananameter.batchclip.plist",
   ]
 end
